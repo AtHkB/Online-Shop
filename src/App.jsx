51b938b46dc5e2f1f6bc6389d70dd29c9352c8a8
nightWolf666 <chsturm75@gmail.com>
2024-03-25 11:53:42 +0100

    basic navbar with example navitems added and App.jsx cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,7 @@ function App() {
   return (
     <>
       <Navbar />
-      <h1 className='text-3xl font-bold underline text-red-500'>Vite + React</h1>
-      <div >
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p >
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
