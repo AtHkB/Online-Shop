fae173328e9f926e0b5e690b4d1ce30b27727a9e
nightWolf666 <chsturm75@gmail.com>
2024-03-25 11:40:32 +0100

    basic navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar />
       <h1 className='text-3xl font-bold underline text-red-500'>Vite + React</h1>
       <div >
         <button onClick={() => setCount((count) => count + 1)}>
